bfead70e0505e840456bb1dab74627b365270860
Alex Foster <alexfost94@gmail.com>
2021-05-14 10:55:36 -0600

    Needed to npm i jquery & popper.js

--- src/components/AddTask.js
+++ src/components/AddTask.js
@@ -3,23 +3,23 @@ import { useState } from "react";
 const AddTask = () => {
   const [text, setText] = useState("");
   const [day, setDay] = useState("");
-  const [reminder, setReminder] = useState("false");
+  const [reminder, setReminder] = useState(false);
   return (
-    <form>
-      <div class="mb-3">
+    <form className="col-sm-12">
+      <div className="mb-3">
         <label for="exampleInputEmail1" class="form-label">
           Enter the title of the task below
         </label>
         <input
           type="text"
-          class="form-control"
+          className="form-control"
           id="text"
           placeholder="Title"
           value={text}
           onChange={(e) => setText(e.target.value)}
         />
       </div>
-      <div class="mb-3">
+      <div className="mb-3">
         <label>Enter the date and time below</label>
         <input
           type="text"
@@ -27,18 +27,18 @@ const AddTask = () => {
           id="day"
           placeholder="Date & Time"
           value={day}
-          onChange={(e) => setText(e.target.value)}
+          onChange={(e) => setDay(e.target.value)}
         />
       </div>
-      <div class="mb-3 form-check">
+      <div class="mb-3 ml-3">
         <input
           type="checkbox"
           class="form-check-input"
           id="exampleCheck1"
           value={reminder}
-          onChange={(e) => setText(e.currentTarget.checked)}
+          onChange={(e) => setReminder(e.currentTarget.checked)}
         />
-        <label class="form-check-label">Reminder</label>
+        <label class="form-check-label ml-3">Reminder?</label>
       </div>
       <button type="submit" class="btn btn-success btn-block">
         Submit
